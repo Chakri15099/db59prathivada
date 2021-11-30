1a212133f1e34e80e70c8de653f1855a5606f0dd
Chakri15099 <chakradhar.prathivada1509@gmail.com>
2021-11-30 12:00:01 -0600

    added bonus

--- routes/bat.js
+++ routes/bat.js
@@ -24,12 +24,12 @@ router.get('/detail', bat_controlers.bat_view_one_Page);
 
 
  /* GET create bat page */ 
-router.get('/create', bat_controlers.bat_create_Page); 
+router.get('/create',secured, bat_controlers.bat_create_Page); 
 
 /* GET create update page */ 
 router.get('/update',secured, bat_controlers.bat_update_Page); 
 
 /* GET create bat page */ 
-router.get('/delete', bat_controlers.bat_delete_Page); 
+router.get('/delete',secured, bat_controlers.bat_delete_Page); 
 
 module.exports = router;
